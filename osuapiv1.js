55d5d7d87585a24d6087c47c7894d458242241fb
simp sylphiette <61514399+Varkaria@users.noreply.github.com>
2021-05-13 01:57:34 +0700

    Initial commit

---
+++ osuapiv1.js
@@ -0,0 +1,31 @@
+const osuapiurl = 'https://osu.ppy.sh/p/api' // Current osu! API url
+const osuavatarurl = 'https://a.ppy.sh' // Current osu! Avatar url
+
+// 💘 This function will getting about user infomation from username or userid in bancho server
+async function GetUserInfomation(user, apikey) {
+    const response = await fetch(osuapiurl + `/api/get_user?u=${user}&k=${apikey}}`)
+    const data = await response.json()
+    return data[0] // Selecting in first data in array
+}
+
+// 👔 This function will getting about user profile from username or userid in bancho server
+/* Warning : Not Recommended to use GetUserInfomation and GetUserAvatar in the same time
+I recommend to make a varible of GetUserInfomation is better 😇  */
+async function GetUserAvatar(user, apikey) {
+    const userdata = await GetUserInfomation(user, apikey)
+    return `${osuavatarurl}/${userdata.userid}`
+}
+
+// 📜 This function will getting about user best scores from username or userid in bancho server
+async function GetUserBest(user, apikey) {
+    const response = await fetch(osuapiurl + `/api/get_user_best?u=${user}&k=${apikey}}`)
+    const data = await response.json()
+    return data // Selecting in first data in array
+}
+
+// 📜 This function will getting about user recent scores from username or userid in bancho server
+async function GetUserRecent(user, apikey) {
+    const response = await fetch(osuapiurl + `/api/get_user_recent?u=${user}&k=${apikey}}`)
+    const data = await response.json()
+    return data // Selecting in first data in array
+}
